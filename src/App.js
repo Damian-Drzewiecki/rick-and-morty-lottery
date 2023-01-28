4d870c4e6671f483a26bf4c7b646a06acacb7c88
Damian-Drzewiecki <drzewiecki.damian96@gmail.com>
2023-01-28 21:22:11 +0100

    Add background music

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.scss';
 import { useCallback, useEffect, useState } from 'react';
 import logo from './assets/Logo.png';
+import useSound from 'use-sound';
+import backgroundSound from './assets/rickandmortytheme.mp3';
 
 async function fetchAllCharacters() {
   const request = await fetch("https://rickandmortyapi.com/api/character")
@@ -18,9 +20,14 @@ function getRandomCharacterId(maxRange) {
   return Math.floor(Math.random() * (maxRange - 1))
 }
 
+function characterType(type) {
+  if (type === "") { return " -" } else { return type };
+}
+
 export default function App() {
   const [maxCount, setMaxCount] = useState()
   const [character, setCharacter] = useState(null)
+  const [play] = useSound(backgroundSound)
 
   const getRandomCharacter = useCallback(async () => {
     const randomCharacter = await fetchCharacterById(getRandomCharacterId(maxCount))
@@ -38,13 +45,16 @@ export default function App() {
     fetchData()
   }, [])
 
+  useEffect(() => {
+    play()
+  }, [play])
+
 
   return (
     <div>
       <div className="smallStars"></div>
       <div className="mediumStars"></div>
       <div className="bigStars"></div>
-
       <div className='container'>
         <div className="portal_blur"></div>
         <div className="portal"></div>
@@ -52,22 +62,29 @@ export default function App() {
         <div className="portal2"></div>
         <div className="portal3"></div>
         <div className="portal4"></div>
-
-        <img className="logo" src={logo} alt="Logo" />
-        <img className="characterImg" src={character?.image} alt="" onClick={() => getRandomCharacter()}></img>
+        <div className="imgFade">
+          <img className="characterImg" src={character?.image} alt="" onClick={() => getRandomCharacter()}></img>
+        </div>
+        <div className="logoFade">
+          <img className="logo" src={logo} alt="Logo" />
+        </div>
 
         <div className="characterProfile">
           <div className="profileData1">
-            <p>ID:{character?.id}</p>
-            <p>NAME:{character?.name}</p>
-            <p>GENDER:{character?.gender}</p>
+            <p className='p1'>ID:{" "}{character?.id}</p>
+            <p className='p2'>NAME:{character?.name}</p>
+            <p className='p3'>GENDER:{character?.gender}</p>
           </div>
           <div className="profileData2">
-            <p>SPECIES:{character?.species}</p>
-            <p>STATUS:{character?.status}</p>
-            <p>TYPE:{character?.type}</p>
+            <p className='p4'>SPECIES:{character?.species}</p>
+            <p className='p5'>STATUS:{character?.status}</p>
+            <p className='p6'>TYPE:{characterType(character?.type)}</p>
+            <div className="p7">
+              <p className='dot'>█</p>
+            </div>
           </div>
         </div>
+
       </div>
     </div>
   )
