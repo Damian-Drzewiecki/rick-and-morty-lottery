5d612bb1d831812772f62b1b86775eeac2d53339
vvatom <drzewiecki.damian96@gmail.com>
2023-03-08 19:29:45 +0100

    Add intro

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import backgroundSound from './assets/rickandmortytheme.mp3';
 import Portal from './components/Portal';
 import Profile from './components/Profile';
 import Logo from './components/Logo';
+import classNames from 'classnames';
 
 async function fetchAllCharacters() {
   const request = await fetch("https://rickandmortyapi.com/api/character")
@@ -26,6 +27,7 @@ export default function App() {
   const [maxCount, setMaxCount] = useState()
   const [character, setCharacter] = useState(null)
   const [imgClick, setImgClick] = useState()
+  const [isInitialized, setIsInitialized] = useState(false)
 
   const getRandomCharacter = useCallback(async () => {
     const randomCharacter = await fetchCharacterById(getRandomCharacterId(maxCount))
@@ -46,15 +48,22 @@ export default function App() {
   return (
     <div>
       <ReactAudioPlayer
-        src={backgroundSound}
+        src={isInitialized ? backgroundSound : ""}
         autoPlay
-        loop="true"
+        loop
       />
+
       <div className="stars stars--small"></div>
       <div className="stars stars--medium"></div>
       <div className="stars stars--big"></div>
 
-      <div className='container'>
+      <div className='startQuestion'>
+        <div className={classNames("startQuestion__Question",{'unInitialized': isInitialized})}>Want to load data?</div>
+        <div onClick={() => setIsInitialized(true)} className={classNames("startQuestion__YES",{'unInitialized': isInitialized})}>YES</div>
+        <div onClick={() => setIsInitialized(true)} className={classNames("startQuestion__OK",{'unInitialized': isInitialized})}>OK</div>
+        </div>
+
+      <div className={classNames('container',{'unInitialized': !isInitialized})}>
         <Portal character={character} setImgClick={setImgClick} getRandomCharacter={getRandomCharacter} />
         <Profile character={character} imgClick={imgClick} />
         <Logo />
