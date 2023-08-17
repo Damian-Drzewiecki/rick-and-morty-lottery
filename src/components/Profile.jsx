8add3777aed61fdda0ebbd033db4c4a546466172
vvatom <112102207+vvatom@users.noreply.github.com>
2023-08-17 15:39:05 +0200

    fix load data animation bug

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,28 +7,28 @@ export default function Profile(props) {
     return (
         <div className="character">
             <div className="character__data_one">
-                <div className={imgClick ? "character__id--anim" : ""}>
-                    <p className="character__id">ID:{" "}{character?.id}</p>
+                <div className="character__id">
+                    <p className={imgClick ? "character__id--anim" : ""} >ID:{" "}{character?.id}</p>
                 </div>
-                <div className={imgClick ? "character__name--anim" : ""}>
-                    <p className="character__name">NAME:{character?.name}</p>
+                <div className="character__name">
+                    <p className={imgClick ? "character__name--anim" : ""} >NAME:{character?.name}</p>
                 </div>
-                <div className={imgClick ? "character__gender--anim" : ""}>
-                    <p className="character__gender">GENDER:{character?.gender}</p>
+                <div className="character__gender">
+                    <p className={imgClick ? "character__gender--anim" : ""} >GENDER:{character?.gender}</p>
                 </div>
             </div>
             <div className="character__data_two">
-                <div className={imgClick ? "character__species--anim" : ""}>
-                    <p className="character__species">SPECIES:{character?.species}</p>
+                <div className="character__species">
+                    <p className={imgClick ? "character__species--anim" : ""} >SPECIES:{character?.species}</p>
                 </div>
-                <div className={imgClick ? "character__status--anim" : ""}>
-                    <p className="character__status">STATUS:{character?.status}</p>
+                <div className="character__status">
+                    <p className={imgClick ? "character__status--anim" : ""} >STATUS:{character?.status}</p>
                 </div>
-                <div className={imgClick ? "character__type--anim" : ""}>
-                    <p className="character__type">TYPE:{character?.type || " -"}</p>
+                <div className="character__type">
+                    <p className={imgClick ? "character__type--anim" : ""} >TYPE:{character?.type || " -"}</p>
                 </div>
-                <div className={imgClick ? "character__dot--anim" : ""}>
-                    <div className="character__dot">
+                <div className="character__dot">
+                    <div className={imgClick ? "character__dot--anim" : ""} >
                         <p className='character__dot--flashes'>█</p>
                     </div>
                 </div>
